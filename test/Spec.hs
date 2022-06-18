62111803587873f0edd40047361bd7151cea00c1
Ometita Radu Adrian <radu.ometita@gmail.com>
2022-06-18 14:56:47 +0300

    Added a test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,23 @@
+module Main where
+
+import           Data.Either.Validation  as V
+import           Test.QuickCheck.Monadic
+import           Test.Tasty
+import           Test.Tasty.QuickCheck
+
+import           Generator
+import           Model
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests"
+  [ testProperty "Generated chains are valid" $ prop_genChainIsValid
+  ]
+
+prop_genChainIsValid :: Chain -> Property
+prop_genChainIsValid chain = property $
+  case validateChain chain of
+    V.Success _ -> True
+    V.Failure _ -> False
